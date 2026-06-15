442b9220181fb0437225ee4eba2f4b02c9f109e5
Luisa Muzzi <luisamuzzi29@gmail.com>
2026-06-15 12:34:45 -0300

    Aula 13

--- src/fundamentos_engenharia_software/preprocessing/feature_engineering.py
+++ src/fundamentos_engenharia_software/preprocessing/feature_engineering.py
@@ -14,251 +14,195 @@ As funções incluem:
 - encode_categorical_columns: Aplica encoder em colunas categóricas
 - create_features_and_encode: Orquestra todas as etapas
 """
+from __future__ import annotations
 
 import pandas as pd
 import numpy as np
+from typing import List, Optional
 from sklearn.preprocessing import LabelEncoder
-from typing import List
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+
 
 from src.fundamentos_engenharia_software.config import (
     RAW_DATA_PATH,
     PROCESSED_DATA_PATH,
 )
 
+class CategoryGrouper(BaseEstimator, TransformerMixin):
+    def __init__(self, percentage_cutoff: int = 80, top_n: int = 685):
+        self.percentage_cutoff = percentage_cutoff
+        self.top_n = top_n
 
-def create_cumulative_fraud_percentage(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Calcula a porcentagem acumulada de fraudes por categoria de produto.
+        self.lista_categoria_outros = []
 
-    Esta função agrega os dados de transações. Vamos contar os registros
-    e total de fraudes por categoria, ordenar o resultado pela contagem
-    e calcular um percentual acumulado.
-
-    :param df: O DataFrame de entrada com os dados de transações.
-    :type df: pd.DataFrame
-    :return: Um novo DataFrame agregado por categoria de produto com o
-             cálculo percentual acumulado de fraudes.
-    :rtype: pd.DataFrame
-    """
-
-    df_copy = df.copy()
-
-    item_cat = df_copy.categoria_produto.value_counts().reset_index()
-    item_cat.columns = ["categoria_produto", "qnt_registros"]
-
-    fraude_cat = (
-        df_copy.groupby("categoria_produto")["fraude"].sum().reset_index()
-    )
-
-    df_item_fraude = pd.merge(
+    def fit(self, X: pd.DataFrame, y: pd.Series) -> CategoryGrouper:
+       df_copy = pd.concat([X, y], axis=1)
+       item_cat = df_copy.categoria_produto.value_counts().reset_index()
+       item_cat.columns = ["categoria_produto", "qnt_registros"]
+       
+       fraude_cat = (
+            df_copy.groupby("categoria_produto")["fraude"].sum().reset_index()
+        )
+       
+       df_item_fraude = pd.merge(
         item_cat, fraude_cat, on="categoria_produto", how="left"
-    )
-
-    df_item_fraude = df_item_fraude.sort_values(
+        )
+       df_item_fraude = df_item_fraude.sort_values(
         by="fraude", ascending=False
-    ).reset_index(drop=True)
-
-    df_item_fraude["percent_cumsum_fraude"] = (
+        ).reset_index(drop=True)
+       
+       df_item_fraude["percent_cumsum_fraude"] = (
         df_item_fraude["fraude"].cumsum() / df_copy["fraude"].sum() * 100
+        )
+       
+       df_item_fraude["reaches_80"] = (
+        df_item_fraude["percent_cumsum_fraude"] <= self.percentage_cutoff
     )
+       
+       produtos_categorias = df_item_fraude[self.top_n:]
+       
+       self.lista_categorias_outros = produtos_categorias.categoria_produto.to_list()
 
-    return df_item_fraude
+       return self
 
+    def transform(self, X:pd.DataFrame) -> pd.DataFrame:
+        X_copy = X.copy()
 
-def extract_least_frequent_categories(
-    df: pd.DataFrame, percentage_cutoff: int = 80, top_n: int = 685
-) -> List[str]:
-    """
-    Extrai uma lista de categorias de produtos menos frequentes.
-
-    A partir de um DataFrame pré-processado e ordenado, essa função
-    retorna uma lista de nomes de categorias a partir de um índice de
-    corte (top_n).
-
-    :param df: O DataFrame de entrada, idealmente processado e ordenado
-               por fraude.
-    :type df: pd.DataFrame
-    :param percentage_cutoff: Ponto de corte percentual para criar uma coluna
-                              de marcação.
-    :type percentage_cutoff: int
-    :param top_n: O índice inicial a partir do qual as características serão
-                  selecionadas.
-    :type top_n: int
-    :return: Uma lista contendo os nomes das categorias menos frequentes.
-    :rtype: List[str]
-    """
-
-    df_copy = df.copy()
-
-    df_copy["reaches_80"] = (
-        df_copy["percent_cumsum_fraude"] <= percentage_cutoff
-    )
-
-    produtos_categorias = df_copy[top_n:]
-
-    lista_categorias_outros = produtos_categorias.categoria_produto.to_list()
-
-    return lista_categorias_outros
-
-
-def create_other_category_values(
-    df: pd.DataFrame, lista_categorias_outros: List[str]
-) -> pd.DataFrame:
-    """
-    Agrupar categorias de produtos menos frequentes em uma única
-    categoria outros.
-
-    :param df: O DataFrame de entrada.
-    :type df: pandas.DataFrame
-    :param lista_categoria_outros: Uma lista contendo categorias
-                                   a serem agrupadas.
-    :type lista_categoria_outros: list of str
-    :returns: Uma cópia do DataFrame com uma nova coluna.
-    :rtype: pandas.DataFrame
-    """
-
-    df_copy = df.copy()
-
-    df_copy["grupo_categorias"] = df_copy["categoria_produto"]
-
-    df_copy.loc[
-        df_copy["grupo_categorias"].isin(lista_categorias_outros),
+        X_copy["grupo_categorias"] = X_copy["categoria_produto"]
+        
+        X_copy.loc[
+        X_copy["grupo_categorias"].isin(self.lista_categorias_outros),
         "grupo_categorias",
-    ] = "categorias_outros"
+        ] = "categorias_outros"
 
-    return df_copy
-
-
-def group_countries(
-    df: pd.DataFrame, countries_to_keep: List[str]
-) -> pd.DataFrame:
-    """
-    Agrupa países em 'Outros', mantendo os da lista repassada.
-
-    Criar a coluna 'paises_agrupados', que contém o valor original da
-    coluna 'pais' se ele estiver na lista 'countries_to_keep', ou
-    'Outros' caso contrário.
-
-    :param df: DataFrame com a coluna 'pais'.
-    :type df: pd.DataFrame
-    :param countries_to_keep: Lista de códigos de países a serem
-                              mantidos.
-    :type countries_to_keep: list of str
-    :returns: Cópia do DataFrame com a nova coluna 'paises_agrupados'.
-    :rtype: pandas.DataFrame
-    """
-
-    df_copy = df.copy()
-
-    df_copy["paises_agrupados"] = np.where(
-        df_copy["pais"].isin(countries_to_keep), df_copy["pais"], "Outros"
-    )
-
-    return df_copy
+        return X_copy
 
 
-def create_document_columns(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Cria novas features baseadas nas colunas de entrega de documentos.
+class CountryGrouper(BaseEstimator, TransformerMixin):
+    def __init__(self, countries_to_keep: List[str]):
+        self.countries_to_keep = countries_to_keep
 
-    Adiciona duas novas colunas:
-    1. 'entrega_doc_2_nan': Flag (1/0) que indica se 'entrega_doc_2' é
-                            nulo.
-    2. 'entrega_doc': Flag que indica se algum dos documentos foi
-                      entregue.
+    def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> CountryGrouper:
+        return self
+    
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X_copy = X.copy()
 
-    :param df: DataFrame com as colunas 'entrega_doc_1',
-               'entrega_doc_2' e 'entrega_doc_3'.
-    :type df: pandas.DataFrame
-    :returns: Cópia do DataFrame com as duas novas colunas de features.
-    :rtype: pandas.DataFrame
-    """
+        X_copy["paises_agrupados"] = np.where(
+        X_copy["pais"].isin(self.countries_to_keep), X_copy["pais"], "Outros"
+        )
 
-    df_copy = df.copy()
-
-    df_copy["entrega_doc_2_nan"] = np.where(
-        df_copy["entrega_doc_2"].isnull(), 1, 0
-    )
-
-    df_copy["entrega_doc"] = (
-        df_copy[["entrega_doc_1", "entrega_doc_2", "entrega_doc_3"]]
-        .any(axis=1)
-        .astype(int)
-    )
-
-    return df_copy
+        return X_copy
 
 
-def encode_categorical_columns(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Codifica colunas categóricas (object) em valores numéricos.
+class DocumentFeatureCreator(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
 
-    Identifica todas as colunas do tipo 'objeto' e as transforma em
-    valores inteiros sequenciais usando 'LabelEncoder'.
+    def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> DocumentFeatureCreator:
+        return self
+    
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        df_copy = X.copy()
 
-    :param df: DataFrame com colunas categóricas a serem codificadas.
-    :type df: pandas.DataFrame
-    :returns: Cópia do DataFrame com as colunas categóricas codificadas.
-    :rtype: pandas.DataFrame
-    """
+        df_copy["entrega_doc_2_nan"] = np.where(
+            df_copy["entrega_doc_2"].isnull(), 1, 0
+        )
 
-    df_copy = df.copy()
+        df_copy["entrega_doc"] = (
+            df_copy[["entrega_doc_1", "entrega_doc_2", "entrega_doc_3"]]
+            .any(axis=1)
+            .astype(int)
+        )
 
-    cat = list(df_copy.select_dtypes(include="object").columns)
+        return df_copy
+        
 
-    le = LabelEncoder()
+class ColumnEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.encoders_ = {}
+        self.columns_ = []
 
-    for col in cat:
-        df_copy.loc[:, col] = le.fit_transform(df_copy[col].astype(str))
+    def fit(self, X: pd.DataFrame, y: Optional[pd.Series] = None) -> ColumnEncoder:
+        self.columns_ = list(X.select_dtypes(include="object").columns)
 
-    return df_copy
+        for col in self.columns_:
+            le = LabelEncoder()
+            le.fit(X[col].astype(str))
+            self.encoders_[col] = le
 
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        X_copy = X.copy()
+
+        for col in self.columns_:
+            if col in self.encoders_:
+                le = self.encoders_[col]
+                X_copy[col] = le.transform(X_copy[col].astype(str))
+
+        return X_copy
+
+
+class FeatureEngineer():
+    def __init__(self, input_path, output_path):
+        self.input_path = input_path
+        self.output_path = output_path
+
+        self.feature_pipeline = self._get_pipeline()
+
+    def _get_pipeline(self) -> Pipeline:
+
+        return Pipeline(
+            steps=[
+                ('category_grouper', CategoryGrouper()),
+                ('country_grouper', CountryGrouper(countries_to_keep=['BR', 'AR'])),
+                ('document_feature_creator', DocumentFeatureCreator()),
+                ('column_encoder', ColumnEncoder())
+            ]
+        )
+    
+    def read_raw_data(self) -> None:
+        try:
+            print("Lendo os dados brutos.")
+            self.data = pd.read_excel(self.input_path)
+            print("Dados lidos com sucesso.")
+        except FileNotFoundError:
+            print(f"O arquivo de entrada não foi encontrado: {self.input_path}")
+            raise Exception
+        except Exception as e:
+            print(f"Ocorreu um erro inesperado ao ler o arquivo de dados: {e}")
+            raise
+
+    def save_feature_engineer_results(self) -> None:
+        try:
+            print("Salvando dados com features.")
+            self.processed_data.to_csv(self.output_path, index=False)
+            print(f"Arquivo salvo em {self.output_path}")
+        except PermissionError:
+            print(
+                f"Sem permissão para escrever o arquivo em {self.output_path}"
+            )
+            raise Exception
+
+    def run_feature_engineer(self) -> None:
+        self.read_raw_data()
+
+        y = self.data['fraude']
+        X = self.data.drop(columns=['fraude'])
+        
+        data_processed = self.feature_pipeline.fit_transform(X, y)
+        data_processed['fraude'] = y.values
+
+        self.processed_data = data_processed
+
+        self.save_feature_engineer_results()
 
 def create_features_and_encode() -> None:
-    """
-    Executa o pipeline completo de feature engineering e encoding.
-
-    Orquestra a leitura dos dados, a criação de features, o
-    agrupamento de categorias, o encoding de variáveis e, por fim,
-    salva o DataFrame processado em um arquivo CSV.
-
-    Não possui parâmetros de entrada nem valor de retorno explícito.
-    Sua principal função é gerar um arquivo CSV com os dados
-    processados.
-    """
-
-    try:
-        print("Lendo os dados brutos.")
-        df = pd.read_excel(RAW_DATA_PATH)
-        print("Dados lidos com sucesso.")
-    except FileNotFoundError:
-        print(f"O arquivo de entrada não foi encontrado: {RAW_DATA_PATH}")
-        raise Exception
-    except Exception as e:
-        print(f"Ocorreu um erro inesperado ao ler o arquivo de dados: {e}")
-        raise
-
-    df_with_percentage_column = create_cumulative_fraud_percentage(df)
-    other_categories_list = extract_least_frequent_categories(
-        df_with_percentage_column, percentage_cutoff=80, top_n=685
-    )
-    df_with_other_category = create_other_category_values(
-        df, other_categories_list
+    
+    feature_engineer = FeatureEngineer(
+        input_path=RAW_DATA_PATH, output_path=PROCESSED_DATA_PATH
     )
 
-    df_with_countries_grouped = group_countries(
-        df_with_other_category, countries_to_keep=["BR", "AR"]
-    )
-    df_with_doc_columns = create_document_columns(df_with_countries_grouped)
-
-    df_with_encoding = encode_categorical_columns(df_with_doc_columns)
-
-    try:
-        print("Salvando dados com features.")
-        df_with_encoding.to_csv(PROCESSED_DATA_PATH, index=False)
-        print(f"Arquivo salvo em {PROCESSED_DATA_PATH}")
-    except PermissionError:
-        print(
-            f"Sem permissão para escrever o arquivo em {PROCESSED_DATA_PATH}"
-        )
-        raise Exception
+    feature_engineer.run_feature_engineer()
